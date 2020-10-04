c899c604320768e70c9bb3d6d30ba78158a8e6f3
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-04 14:55:09 -0300

    added index and contact view

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from pages.views import HomePageView
+from pages.views import ContactPageView, HomePageView, IndexPageView
 
 app_name = 'pages'
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('index/', IndexPageView.as_view(), name='index'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,12 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+
+
+class IndexPageView(TemplateView):
     template_name = 'pages/index.html'
