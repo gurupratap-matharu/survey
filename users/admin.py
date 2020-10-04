046cb6e720599eee0357c4da68e4d34915b21130
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-04 12:05:17 -0300

    added custom user to django admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.forms import CustomUserChangeForm, CustomUserCreationForm
+
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username', ]
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
