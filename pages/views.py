1e3ccfb9e7fb584d1c9057e643e8ff1b2927d407
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-05 14:27:02 -0300

    added contact form in contact and about page

--- pages/views.py
+++ pages/views.py
@@ -1,17 +1,34 @@
-from django.views.generic import TemplateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import FormView, TemplateView
+
+from pages.forms import ContactForm
 
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
 
-class ContactPageView(TemplateView):
+class ContactPageView(SuccessMessageMixin, FormView):
     template_name = 'pages/contact.html'
+    form_class = ContactForm
+    success_message = 'Thanks! We have received your message!'
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
+
+
+class AboutPageView(SuccessMessageMixin, FormView):
+    template_name = 'pages/about.html'
+    form_class = ContactForm
+    success_message = 'Thanks! We have received your message!'
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
 
 
 class IndexPageView(TemplateView):
     template_name = 'pages/index.html'
-
-
-class AboutPageView(TemplateView):
-    template_name = 'pages/about.html'
