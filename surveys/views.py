c5e0c258265296fe182a3a1e5950ef71b3561374
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-05 15:00:13 -0300

    added CRUD views for survey model

--- surveys/views.py
+++ surveys/views.py
@@ -1,17 +1,27 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import DeleteView, DetailView, ListView, UpdateView
+
+from surveys.models import Survey
 
 
 class SurveyList(ListView):
-    pass
+    model = Survey
+    template_name = 'surveys/survey_list.html'
 
 
 class SurveyDetail(DetailView):
-    pass
+    model = Survey
+    template_name = 'surveys/survey_detail.html'
 
 
-class SurveyUpdate(UpdateView):
-    pass
+class SurveyUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Survey
+    template_name = 'surveys/survey_update_form.html'
+    success_message = '%(title)s updated successfully!'
 
 
-class SurveyDelete(DeleteView):
-    pass
+class SurveyDelete(LoginRequiredMixin, DeleteView):
+    model = Survey
+    template_name = 'surveys/survey_confirm_delete.html'
+    success_message = '%s(title) deleted successfully!'
