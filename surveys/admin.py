abee853e2d16e22cc304b70809d8f26b6661dd5a
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-05 14:52:05 -0300

    added custom admin for survey model

--- surveys/admin.py
+++ surveys/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from surveys.models import Survey
 
-admin.site.register(Survey)
+
+class SurveyAdmin(admin.ModelAdmin):
+    model = Survey
+    list_display = ('title', 'is_active', 'author',)
+    list_filter = ('is_active', 'created_on',)
+    search_fields = ('title',)
+    readonly_fields = ('author',)
+
+
+admin.site.register(Survey, SurveyAdmin)
