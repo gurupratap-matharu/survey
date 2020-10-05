00dc1261d2b000f0efbd497303336559f06ed583
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-05 14:47:21 -0300

    survey model with complete details

--- surveys/models.py
+++ surveys/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.urls import reverse
+
+
+class Survey(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='surveys')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('survey_detail', args=[str(self.id)])
+
+    def get_update_url(self):
+        return reverse('survey_update', args=[str(self.id)])
+
+    def get_delete_url(self):
+        return reverse('survey_delete', args=[str(self.id)])
+
+    def get_link(self):
+        return self.get_absolute_url()
+
+    def can_update(self, user):
+        return user.is_superuser or self.author == user
+
+    def can_delete(self, user):
+        return user.is_superuser or self.author == user
