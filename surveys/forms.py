19667ba70b8faea8392381af48b23d20a156a553
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-05-23 14:34:22 -0300

    added larger text widget for survey create form

--- surveys/forms.py
+++ surveys/forms.py
@@ -7,3 +7,6 @@ class SurveyForm(forms.ModelForm):
     class Meta:
         model = Survey
         fields = ('title', 'description',)
+        widgets = {
+            'description': forms.Textarea(attrs={'cols': 80, 'rows': 5}),
+        }
