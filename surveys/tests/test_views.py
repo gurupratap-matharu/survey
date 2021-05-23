3c088c479c64a7691095696fe83935cf5b5f019e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-05-23 14:28:25 -0300

    added unit tests to verify authorized login on survey creation

--- surveys/tests/test_views.py
+++ surveys/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+from surveys.forms import SurveyForm
+from surveys.views import SurveyCreate
 
 
 class SurveyCreateTests(TestCase):
@@ -11,16 +13,28 @@ class SurveyCreateTests(TestCase):
         )
 
     def test_survey_create_view_redirect_for_anonymous_user(self):
-        self.fail()
+        response = self.client.get(reverse('survey_create'))
+        self.assertTemplateNotUsed(response, 'surveys/survey_form.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, expected_url=response.url, status_code=302,
+                             target_status_code=200)
 
     def test_survey_create_view_works_for_logged_in_user(self):
-        self.fail()
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('survey_create'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'surveys/survey_form.html')
 
     def test_survey_create_view_contains_survey_create_form(self):
-        self.fail()
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('survey_create'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'surveys/survey_form.html')
+        self.assertIsInstance(response.context['form'], SurveyForm)
 
     def test_survey_create_view_resolve_SurveyCreateView(self):
-        self.fail()
+        view = resolve(reverse('survey_create'))
+        self.assertEqual(view.func.__name__, SurveyCreate.as_view().__name__)
 
     def test_survey_create_view_uses_correct_html_template(self):
         self.client.force_login(self.user)
