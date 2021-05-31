9627d90baae13c215b21279882f38f3714be1008
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-05-31 20:58:56 -0300

    added conditional block to run debugger in docker container

--- manage.py
+++ manage.py
@@ -15,6 +15,17 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    if (os.getenv("RUN_MAIN") or os.getenv("WERKZEUG_RUN_MAIN")) and os.getenv("VSCODE_DEBUGGER", False):
+        import ptvsd  # noqa
+        ptvsd_port = 4000
+        print('Veer starting debugger...')
+        try:
+            ptvsd.enable_attach(address=("0.0.0.0", ptvsd_port), redirect_output=True)
+            print("Started ptvsd at port %s." % ptvsd_port)
+        except OSError:
+            print("ptvsd port %s already in use." % ptvsd_port)
+
     execute_from_command_line(sys.argv)
 
 
